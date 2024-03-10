1ef8bce18487ec485e6693179dadd6d5d53f17f8
Zhen Yang <zzhenyyang@gmail.com>
2024-03-10 08:55:44 +0800

    fixed commands

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -16,21 +16,3 @@ declare global {
 Cypress.Commands.add("getDataTestid", (value) => {
   return cy.get(`[data-testid=${value}]`);
 });
-
-Cypress.Commands.add("bypassLogin", (email, password) => {
-  cy.session(
-    [email, password],
-    () => {
-      cy.visit("/login");
-      cy.fixture("user").then((user) => {
-        cy.getDataTestid("email-login").type(user.email);
-        cy.getDataTestid("password-login").type(user.password);
-      });
-      cy.getDataTestid("login-btn").click();
-      cy.url().should("match", /transactions/);
-    },
-    {
-      cacheAcrossSpecs: true,
-    }
-  );
-});
