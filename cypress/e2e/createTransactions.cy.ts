8de57fa0ede0433bde3833568c654ba2a393719e
Zhen Yang <zzhenyyang@gmail.com>
2024-09-22 21:00:31 +0800

    fix: flaky button clicks and duplicate select tags

--- cypress/e2e/createTransactions.cy.ts
+++ cypress/e2e/createTransactions.cy.ts
@@ -20,20 +20,19 @@ describe('Create a transaction', () => {
       for(const transaction of transactions){
         cy.visit('/transactions/create')
         // Create transaction
-        cy.getDataTestid('select-category').select(transaction.category)
-        cy.getDataTestid('amount-input').type(transaction.amount)
-        cy.getDataTestid('description-input').type(transaction.description)
-        cy.getDataTestid('date-input')
-        .type(transaction.date)
-        cy.getDataTestid('create-transaction-btn').click()
+        cy.getDataTestid('select-category').filter(':visible').select(transaction.category)
+        cy.getDataTestid('amount-input').filter(':visible').type(transaction.amount)
+        cy.getDataTestid('description-input').filter(':visible').type(transaction.description)
+        cy.getDataTestid('date-input').filter(':visible').type(transaction.date)
+        cy.getDataTestid('create-transaction-btn').filter(':visible').click()
         
         // Read transaction
-        cy.getDataTestid('transaction-category-mobile').should('contain.text', transaction.category)
-        cy.getDataTestid('transaction-amount-mobile').should('contain.text', formatCurrency(transaction.amount*100)) // amount is stored in cents in the database
-        cy.getDataTestid('transaction-description-mobile').should('contain.text', transaction.description)
-        cy.getDataTestid('transaction-date-mobile').should('contain.text', formatDateToLocal(transaction.date.split('T')[0]))
+        cy.getDataTestid('transaction-category-mobile').filter(':visible').should('contain.text', transaction.category)
+        cy.getDataTestid('transaction-amount-mobile').filter(':visible').should('contain.text', formatCurrency(transaction.amount*100)) // amount is stored in cents in the database
+        cy.getDataTestid('transaction-description-mobile').filter(':visible').should('contain.text', transaction.description)
+        cy.getDataTestid('transaction-date-mobile').filter(':visible').should('contain.text', formatDateToLocal(transaction.date.split('T')[0]))
 
-        cy.getDataTestid('delete-transaction-btn').first().click()
+        cy.getDataTestid('delete-transaction-btn').filter(':visible').first().click()
       }
     })
   })
@@ -44,14 +43,13 @@ describe('Create a transaction', () => {
         cy.visit('/transactions/create')
         // Create transaction
         // cy.getDataTestid('select-category').select(transaction.category) // No category chosen
-        cy.getDataTestid('amount-input').type(transaction.amount)
-        cy.getDataTestid('description-input').type(transaction.description)
-        cy.getDataTestid('date-input')
-        .type(transaction.date)
-        cy.getDataTestid('create-transaction-btn').click()
+        cy.getDataTestid('amount-input').filter(':visible').type(transaction.amount)
+        cy.getDataTestid('description-input').filter(':visible').type(transaction.description)
+        cy.getDataTestid('date-input').filter(':visible').type(transaction.date)
+        cy.getDataTestid('create-transaction-btn').filter(':visible').click()
         
         // Ensure error gets displayed
-        cy.getDataTestid('category-error').should('contain.text', 'Please select a category.')
+        cy.getDataTestid('category-error').filter(':visible').should('contain.text', 'Please select a category.')
       }
     })
   })
@@ -61,15 +59,14 @@ describe('Create a transaction', () => {
       for(const transaction of transactions){
         cy.visit('/transactions/create')
         // Create transaction
-        cy.getDataTestid('select-category').select(transaction.category) // No category chosen
+        cy.getDataTestid('select-category').filter(':visible').select(transaction.category) // No category chosen
         // cy.getDataTestid('amount-input').type(transaction.amount)
-        cy.getDataTestid('description-input').type(transaction.description)
-        cy.getDataTestid('date-input')
-        .type(transaction.date)
-        cy.getDataTestid('create-transaction-btn').click()
+        cy.getDataTestid('description-input').filter(':visible').type(transaction.description)
+        cy.getDataTestid('date-input').filter(':visible').type(transaction.date)
+        cy.getDataTestid('create-transaction-btn').filter(':visible').click()
         
         // Ensure error gets displayed
-        cy.getDataTestid('amount-error').should('contain.text', 'Amount must not be equal to 0')
+        cy.getDataTestid('amount-error').filter(':visible').should('contain.text', 'Amount must not be equal to 0')
       }
     })
   })
@@ -79,15 +76,14 @@ describe('Create a transaction', () => {
       for(const transaction of transactions){
         cy.visit('/transactions/create')
         // Create transaction
-        cy.getDataTestid('select-category').select(transaction.category) // No category chosen
-        cy.getDataTestid('amount-input').type(transaction.amount)
+        cy.getDataTestid('select-category').filter(':visible').select(transaction.category) // No category chosen
+        cy.getDataTestid('amount-input').filter(':visible').type(transaction.amount)
         // cy.getDataTestid('description-input').type(transaction.description)
-        cy.getDataTestid('date-input')
-        .type(transaction.date)
-        cy.getDataTestid('create-transaction-btn').click()
+        cy.getDataTestid('date-input').filter(':visible').type(transaction.date)
+        cy.getDataTestid('create-transaction-btn').filter(':visible').click()
         
         // Ensure error gets displayed
-        cy.getDataTestid('description-error').should('contain.text', 'Please enter a description.')
+        cy.getDataTestid('description-error').filter(':visible').should('contain.text', 'Please enter a description.')
       }
     })
   })
@@ -97,14 +93,14 @@ describe('Create a transaction', () => {
       for(const transaction of transactions){
         cy.visit('/transactions/create')
         // Create transaction with a long description
-        cy.getDataTestid('select-category').select(transaction.category)
-        cy.getDataTestid('amount-input').type(transaction.amount)
-        cy.getDataTestid('description-input').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.') // Example of a long description
-        cy.getDataTestid('date-input').type(transaction.date)
-        cy.getDataTestid('create-transaction-btn').click()
+        cy.getDataTestid('select-category').filter(':visible').select(transaction.category)
+        cy.getDataTestid('amount-input').filter(':visible').type(transaction.amount)
+        cy.getDataTestid('description-input').filter(':visible').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.') // Example of a long description
+        cy.getDataTestid('date-input').filter(':visible').type(transaction.date)
+        cy.getDataTestid('create-transaction-btn').filter(':visible').click()
         
         // Ensure error gets displayed
-        cy.getDataTestid('description-error').should('contain.text', 'String must contain at most 25 character(s)')
+        cy.getDataTestid('description-error').filter(':visible').should('contain.text', 'String must contain at most 25 character(s)')
       }
     })
   })
@@ -114,11 +110,11 @@ describe('Create a transaction', () => {
       for(const transaction of transactions){
         cy.visit('/transactions/create')
         // Create transaction with a long description
-        cy.getDataTestid('select-category').select(transaction.category)
-        cy.getDataTestid('amount-input').type(`${transaction.amount}23`)
-        cy.getDataTestid('description-input').type(transaction.description) // Example of a long description
-        cy.getDataTestid('date-input').type(transaction.date)
-        cy.getDataTestid('create-transaction-btn').click()
+        cy.getDataTestid('select-category').filter(':visible').select(transaction.category)
+        cy.getDataTestid('amount-input').filter(':visible').type(`${transaction.amount}23`)
+        cy.getDataTestid('description-input').filter(':visible').type(transaction.description) // Example of a long description
+        cy.getDataTestid('date-input').filter(':visible').type(transaction.date)
+        cy.getDataTestid('create-transaction-btn').filter(':visible').click()
         
         // Ensure input doesnt go through
         cy.url().should('match', /transactions\/create/)
--- cypress/e2e/updateTransactions.cy.ts
+++ cypress/e2e/updateTransactions.cy.ts
@@ -34,26 +34,26 @@ describe("Create a transaction", () => {
       const transaction = transactions[0];
       cy.visit("/transactions/create");
       // Create transaction
-      cy.getDataTestid("select-category").select(transaction.category);
-      cy.getDataTestid("amount-input").type(transaction.amount);
-      cy.getDataTestid("description-input").type(transaction.description);
-      cy.getDataTestid("date-input").type(transaction.date);
-      cy.getDataTestid("create-transaction-btn").click();
+      cy.getDataTestid("select-category").filter(':visible').select(transaction.category);
+      cy.getDataTestid("amount-input").filter(':visible').type(transaction.amount);
+      cy.getDataTestid("description-input").filter(':visible').type(transaction.description);
+      cy.getDataTestid("date-input").filter(':visible').type(transaction.date);
+      cy.getDataTestid("create-transaction-btn").filter(':visible').click();
 
       // Read transaction
-      cy.getDataTestid("transaction-category-mobile").should(
+      cy.getDataTestid("transaction-category-mobile").filter(':visible').should(
         "contain.text",
         transaction.category
       );
-      cy.getDataTestid("transaction-amount-mobile").should(
+      cy.getDataTestid("transaction-amount-mobile").filter(':visible').should(
         "contain.text",
         formatCurrency(transaction.amount * 100)
       ); // amount is stored in cents in the database
-      cy.getDataTestid("transaction-description-mobile").should(
+      cy.getDataTestid("transaction-description-mobile").filter(':visible').should(
         "contain.text",
         transaction.description
       );
-      cy.getDataTestid("transaction-date-mobile").should(
+      cy.getDataTestid("transaction-date-mobile").filter(':visible').should(
         "contain.text",
         formatDateToLocal(transaction.date.split("T")[0])
       );
@@ -67,7 +67,7 @@ describe("Create a transaction", () => {
       cy.visit(`/transactions?month=${new Date(transaction.date).getMonth()+1}&&year=${new Date(transaction.date).getFullYear()}`)
   
       // Delete transaction created for testing update
-      cy.getDataTestid("delete-transaction-btn").first().click();
+      cy.getDataTestid("delete-transaction-btn").filter(':visible').first().click();
     })
   })
 
@@ -78,22 +78,23 @@ describe("Create a transaction", () => {
       cy.visit(`/transactions?month=${new Date(transaction.date).getMonth()+1}&&year=${new Date(transaction.date).getFullYear()}`)
 
       // Read transaction
-      cy.getDataTestid('transaction-category-mobile').should('contain.text', transaction.category)
-      cy.getDataTestid('transaction-amount-mobile').should('contain.text', formatCurrency(transaction.amount*100)) // amount is stored in cents in the database
-      cy.getDataTestid('transaction-description-mobile').should('contain.text', transaction.description)
-      cy.getDataTestid('transaction-date-mobile').should('contain.text', formatDateToLocal(transaction.date.split('T')[0]))
+      cy.getDataTestid('transaction-category-mobile').filter(':visible').should('contain.text', transaction.category)
+      cy.getDataTestid('transaction-amount-mobile').filter(':visible').should('contain.text', formatCurrency(transaction.amount*100)) // amount is stored in cents in the database
+      cy.getDataTestid('transaction-description-mobile').filter(':visible').should('contain.text', transaction.description)
+      cy.getDataTestid('transaction-date-mobile').filter(':visible').should('contain.text', formatDateToLocal(transaction.date.split('T')[0]))
       
-      cy.getDataTestid("update-transaction-btn").first().should('be.visible').click();
+      cy.wait(200)
+      cy.getDataTestid("update-transaction-btn").filter(':visible').first().should('be.visible').click();
       cy.url().should("match", /transactions\/\d+\/edit/);
 
       // Edit transaction
-      cy.getDataTestid("amount-input").clear().type("-999.99");
-      cy.getDataTestid("confirm-update-btn").click();
+      cy.getDataTestid("amount-input").filter(':visible').clear().type("-999.99");
+      cy.getDataTestid("confirm-update-btn").filter(':visible').click();
 
       cy.url().should("not.match", /edit/);
       cy.url().should("match", /transactions/);
 
-      cy.getDataTestid('transaction-amount-mobile').should('contain.text', formatCurrency(-999.99*100)) // amount is stored in cents in the database
+      cy.getDataTestid('transaction-amount-mobile').filter(':visible').should('contain.text', formatCurrency(-999.99*100)) // amount is stored in cents in the database
     })
   });
 
@@ -102,15 +103,16 @@ describe("Create a transaction", () => {
       const transaction = transactions[0];
 
       cy.visit(`/transactions?month=${new Date(transaction.date).getMonth()+1}&&year=${new Date(transaction.date).getFullYear()}`)
-      cy.getDataTestid("update-transaction-btn").first().should('be.visible').click();
+      cy.wait(200)
+      cy.getDataTestid("update-transaction-btn").filter(':visible').first().should('be.visible').click();
       cy.url().should("match", /transactions\/\d+\/edit/);
 
       // Update transaction
-      cy.getDataTestid('amount-input').clear();
-      cy.getDataTestid("confirm-update-btn").click();
+      cy.getDataTestid('amount-input').filter(':visible').clear();
+      cy.getDataTestid("confirm-update-btn").filter(':visible').click();
 
       // Ensure error gets displayed
-      cy.getDataTestid("amount-error").should(
+      cy.getDataTestid("amount-error").filter(':visible').should(
         "contain.text",
         "Amount must not be equal to 0"
       );
@@ -122,15 +124,17 @@ describe("Create a transaction", () => {
       const transaction = transactions[0];
 
       cy.visit(`/transactions?month=${new Date(transaction.date).getMonth()+1}&&year=${new Date(transaction.date).getFullYear()}`)
-      cy.getDataTestid("update-transaction-btn").first().should('be.visible').click();
+      
+      cy.wait(200)
+      cy.getDataTestid("update-transaction-btn").filter(':visible').first().should('be.visible').click();
       cy.url().should("match", /transactions\/\d+\/edit/);
 
       // Update transaction
-      cy.getDataTestid('description-input').clear();
-      cy.getDataTestid("confirm-update-btn").click();
+      cy.getDataTestid('description-input').filter(':visible').clear();
+      cy.getDataTestid("confirm-update-btn").filter(':visible').click();
 
       // Ensure error gets displayed
-      cy.getDataTestid("description-error").should(
+      cy.getDataTestid("description-error").filter(':visible').should(
         "contain.text",
         "Please enter a description."
       );
@@ -142,17 +146,19 @@ describe("Create a transaction", () => {
       const transaction = transactions[0];
 
       cy.visit(`/transactions?month=${new Date(transaction.date).getMonth()+1}&&year=${new Date(transaction.date).getFullYear()}`)
-      cy.getDataTestid("update-transaction-btn").first().should('be.visible').click();
+
+      cy.wait(200)
+      cy.getDataTestid("update-transaction-btn").filter(':visible').first().should('be.visible').click();
       cy.url().should("match", /transactions\/\d+\/edit/);
 
       // Update transaction with a long description
-      cy.getDataTestid("description-input").clear().type(
+      cy.getDataTestid("description-input").filter(':visible').clear().type(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
       ); // Example of a long description
-      cy.getDataTestid("confirm-update-btn").click();
+      cy.getDataTestid("confirm-update-btn").filter(':visible').click();
 
       // Ensure error gets displayed
-      cy.getDataTestid("description-error").should(
+      cy.getDataTestid("description-error").filter(':visible').should(
         "contain.text",
         "String must contain at most 25 character(s)"
       );
@@ -164,12 +170,14 @@ describe("Create a transaction", () => {
       const transaction = transactions[0];
 
       cy.visit(`/transactions?month=${new Date(transaction.date).getMonth()+1}&&year=${new Date(transaction.date).getFullYear()}`)
-      cy.getDataTestid("update-transaction-btn").first().should('be.visible').click();
+
+      cy.wait(200)
+      cy.getDataTestid("update-transaction-btn").filter(':visible').first().should('be.visible').click();
       cy.url().should("match", /transactions\/\d+\/edit/);
 
       // Create transaction with a long description
-      cy.getDataTestid("amount-input").clear().type(`${transaction.amount}23`);
-      cy.getDataTestid("confirm-update-btn").click();
+      cy.getDataTestid("amount-input").filter(':visible').clear().type(`${transaction.amount}23`);
+      cy.getDataTestid("confirm-update-btn").filter(':visible').click();
 
       // Ensure input doesnt go through
       cy.url().should("match", /transactions\/\d+\/edit/);
